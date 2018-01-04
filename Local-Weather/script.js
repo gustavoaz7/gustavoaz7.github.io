8cf76681bb487e566f21809471d529f72668d220
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2018-01-04 10:54:10 -0200

    Fix background size
    Also removed commented image links

--- Local-Weather/script.js
+++ Local-Weather/script.js
@@ -117,6 +117,7 @@ function getAllData(){
         let [i, bg] = getIconAndBackground(main, id, icon, descript);
         weatherIcon.classList.add(i);
         document.body.style.background = `url(${bg}) center no-repeat`;
+        document.body.style.backgroundSize = 'cover';
 
         // Getting 5 Day Forecast
         let forecastURL = 
@@ -178,88 +179,3 @@ function getIconAndBackground(main, id, icon, descript) {
       [sel.other[0], sel.other[1]];
   } else {return [weather.xxx[0], weather.xxx[1]];}
 }
-
-/*
-const weather = {
-  'Thunderstorm': ['wi-thunderstorm', 'url'],
-  'Drizzle': ['wi-sprinkle', 'url'],
-  'Rain': ['wi-rain', 'url'],
-  'Snow': ['wi-snow', 'url'],
-  'Atmosphere': ['wi-dust', 'url'],
-  '800': [['wi-day-sunny', 'url'], ['wi-night-clear', 'url']],
-  '8xx': {'fc': [['wi-day-cloudy', 'url'], ['wi-night-alt-cloudy', 'url']], 'other': ['wi-cloudy', 'url']},
-  'xxx': ['wi-na', 'url']
-}
-
-THUNDERSTORM
-https://www.pixelstalk.net/wp-content/uploads/2016/08/Lightning-Storm-Background-Download-Free.jpg
-https://www.pixelstalk.net/wp-content/uploads/2016/08/Blue-Lightning-Storm-Background.jpg
-http://vunature.com/wp-content/uploads/2016/10/sky-lightning-nature-clouds-storm-rain-thunderstorm-hd-1920x1080-1920x1080.jpg
-* http://cdn.wallpapersafari.com/77/14/gq0rz6.jpg
-http://mmbiz.qpic.cn/mmbiz/9MjD2VejKrn5OPxOhicp2bL0jePBxGpoyAaIVl23bZMX8CfzEqwiag75sVhGypejrlDhvwdnW74Jdc4kP4O78LIw/0?wx_fmt=jpeg
-http://wtop.com/wp-content/uploads/2014/07/355929-1865x1254.jpg
-
-
-    DRIZZLE
-    https://get.pxhere.com/photo/water-nature-drop-sunset-night-texture-rain-window-reflection-macro-weather-closeup-clouds-drops-drops-of-water-the-background-dark-clouds-rain-drops-pane-after-the-storm-drop-of-rain-893238.jpg
-    * https://images5.alphacoders.com/403/403926.jpg
-    http://orig06.deviantart.net/b6f6/f/2012/233/5/6/rainy_wallpaper_by_moggget-d5bwqju.jpg
-
-RAIN
-https://graphiccloud.net/wp-content/uploads/2016/04/rain-blue-drop-background.jpg
-http://cdn.wallpapersafari.com/16/94/oa3LSD.jpg
-https://i.ytimg.com/vi/HopSpWYmVso/maxresdefault.jpg
-* http://www.wallpaperawesome.com/wallpapers-awesome/wallpapers-weather-clouds-tornado-rain-cyclone-flashlights-awesome/wallpaper-drops-of-rain-from-sky-weather.jpg
-http://www.weatherwizkids.com/wp-content/uploads/2015/02/rain21.jpg
-
-    SNOW
-    http://static.independent.co.uk/s3fs-public/thumbnails/image/2016/01/10/16/uk-snow-getty.jpg
-    * http://www.wallpaperawesome.com/wallpapers-awesome/wallpapers-weather-clouds-tornado-rain-cyclone-flashlights-awesome/wallpaper-snowing-in-the-trees-weather.jpg
-    http://99px.ru/sstorage/53/2016/02/tmb_157247_6142.jpg
-
-    MIST
-    * http://cdn.wallpapersafari.com/5/85/z30Ag8.jpg
-    https://i.pinimg.com/originals/79/5e/08/795e08ccde3b67f80119597d74fde0b1.jpg
-
-    CLEAR SKY
-        DAY
-    http://cdn.wallpapersafari.com/36/71/xzgvI2.jpg
-    https://i.ytimg.com/vi/WdcuOC3oyhk/maxresdefault.jpg
-    https://balancebox.files.wordpress.com/2012/04/dsc_0494-edit.jpg
-    https://www.walldevil.com/wallpapers/a83/sun-sky-cloud-field-tree.jpg
-    * https://sheridantower.files.wordpress.com/2012/05/clear-sky7.jpg
-    https://static.squarespace.com/static/53bd3460e4b07d8e4ad42994/t/53d05575e4b0b1c1fde219b1/1406162293119/wallpapers-sky-clear-grasslands-hd-1280x800.jpg
-
-        NIGHT
-    * https://www.walldevil.com/wallpapers/a57/sky-moon-night-star.jpg
-    http://natbg.com/wp-content/uploads/2016/06/sky-bright-glowing-moon-ocean-night-clear-sky-nature-trees-shore-desktop-background-images.jpg
-    http://cdnau.ibtimes.com/sites/au.ibtimes.com/files/2015/12/11/full-moon-rises-above-graveney-kent-clear-autumn-evening-night-descends-britain-october-26-2015.jpg
-    https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/moon-and-misty-night-sky_zjkrmh3gs__F0000.png
-    https://static.pexels.com/photos/8170/sky-clouds-trees-moon.jpg
-
-
-    CLOUDY
-        DAY
-    * http://wpnature.com/wp-content/uploads/2017/04/natural-force-storm-sun-rays-new-york-rochester-clouds-beforethestorm-wallpaper-hd-1920x1080.jpg
-    https://wallpaperscraft.com/image/clouds_sky_sun_beams_day_26843_1600x1200.jpg
-    http://renatures.com/wp-content/uploads/2017/03/sky-gold-cool-beautiful-brown-blue-nice-morning-day-awesome-orange-clouds-sunset-amazing-afternoon-black-nature-white-sun-beauty-forces-sunlight-ipad-wallpaper-night-1600x1080.jpg
-
-        NIGHT
-    * http://eskipaper.com/images/night-clouds-moon-1.jpg
-    http://8-themes.com/wp-content/uploads/2015/12/Space_Moon_among_the_night_clouds_094737_.jpg
-    https://i.pinimg.com/originals/c7/31/73/c731730ed01a1f35c9c5cee252e3180c.jpg
-    https://curiousvogel.files.wordpress.com/2014/07/07132009.jpg
-
-
-        TOTAL
-    http://www.patternpictures.com/wp-content/uploads/2013/12/PP58062412-Radiant-Cloudy-Sky-over-Sea-Water.jpg
-    * http://www.footwa.com/wp-content/uploads/2010/05/Solitary-bird-in-the-sky.jpg
-    http://cdn.wallpapersafari.com/47/67/kN7Vzv.jpg
-
-
-
-
-
-
-
-*/
